5f1f0e4cf0e1622332d7a3ec08ca18ace288c001
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-12-06 00:57:31 +0100

    Miscfunction done

--- src/main/java/fr/java/bases/MiscFunctions.java
+++ src/main/java/fr/java/bases/MiscFunctions.java
@@ -1,24 +1,45 @@
 package fr.java.bases;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
 
 public class MiscFunctions {
-    
+
     public static <T> Long count(List<T> originalLst) {
-        throw new NotImplementedException("Please complete fr.java.bases.MiscFunctions#count");
+        Long count = 0L;
+        for (T elemenT : originalLst) {
+            count++;
+        }
+        return count;
     }
-    
+
     public static <T> List<T> distinct(List<T> originalLst) {
-        throw new NotImplementedException("Please complete fr.java.bases.MiscFunctions#distinct");
+        Set<T> collected = new HashSet<>();
+        List<T> result = new ArrayList<>();
+        originalLst.forEach(element -> {
+            if (!collected.contains(element)) {
+                collected.add(element);
+                result.add(element);
+            }
+        });
+        return result;
     }
-    
+
     public static <T> Optional<T> findAny(List<T> originalLst) {
-        throw new NotImplementedException("Please complete fr.java.bases.MiscFunctions#findAny");
+        if (originalLst.isEmpty())
+            return Optional.empty();
+        int i = 0; // should be non-deterministic (random ?)
+        return Optional.of(originalLst.get(i));
     }
-    
+
     public static <T> Optional<T> findFirst(List<T> originalLst) {
-        throw new NotImplementedException("Please complete fr.java.bases.MiscFunctions#findFirst");
+        if (originalLst.isEmpty())
+            return Optional.empty();
+        return Optional.of(originalLst.get(0));
     }
-    
+
 }
