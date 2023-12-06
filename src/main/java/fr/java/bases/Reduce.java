a368232f16ce70808a031b57662f462e001d2b16
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-12-06 01:09:49 +0100

    reduce

--- src/main/java/fr/java/bases/Reduce.java
+++ src/main/java/fr/java/bases/Reduce.java
@@ -5,9 +5,16 @@ import java.util.Optional;
 import java.util.function.BinaryOperator;
 
 public class Reduce {
-    
+
     public static <T> Optional<T> reduce(List<T> originalLst, BinaryOperator<T> accumulator) {
-        throw new NotImplementedException("Please complete fr.java.bases.Reduce#reduce");
+        if (originalLst.isEmpty())
+            return Optional.empty();
+        T result = originalLst.get(0);
+        for (int i = 1; i < originalLst.size(); i++) {
+            result = accumulator.apply(result, originalLst.get(i));
+        }
+        ;
+        return Optional.of(result);
     }
-    
+
 }
