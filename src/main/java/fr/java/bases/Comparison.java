88d9aa95cd0e2f2bad93136d159cae94f75c4e79
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-12-06 01:04:38 +0100

    min,max done

--- src/main/java/fr/java/bases/Comparison.java
+++ src/main/java/fr/java/bases/Comparison.java
@@ -4,13 +4,34 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
+// peut subir refactoring (duplication. Tu le vois ?) ! 
 public class Comparison {
-    
+
     public static <T> Optional<T> min(List<T> originalLst, Comparator<? super T> comparator) {
-        throw new NotImplementedException("Please complete fr.java.bases.Comparison#min");
+        if (originalLst.isEmpty())
+            return Optional.empty();
+        T currentMin = originalLst.get(0);
+        for (T element : originalLst) {
+            Boolean newMinFound = comparator.compare(currentMin, element) > 0;
+            if (newMinFound) {
+                currentMin = element;
+            }
+        }
+        ;
+        return Optional.of(currentMin);
     }
-    
+
     public static <T> Optional<T> max(List<T> originalLst, Comparator<? super T> comparator) {
-        throw new NotImplementedException("Please complete fr.java.bases.Comparison#max");
+        if (originalLst.isEmpty())
+            return Optional.empty();
+        T currentMax = originalLst.get(0);
+        for (T element : originalLst) {
+            Boolean newMaxFound = comparator.compare(currentMax, element) < 0;
+            if (newMaxFound) {
+                currentMax = element;
+            }
+        }
+        ;
+        return Optional.of(currentMax);
     }
 }
