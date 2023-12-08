c82ffe500c7406434385cb8f6549950f548fbacc
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-12-08 19:43:06 +0100

    finish him

--- src/main/java/fr/java/bases/FinishHim.java
+++ src/main/java/fr/java/bases/FinishHim.java
@@ -7,12 +7,18 @@ import java.util.function.Predicate;
 
 public class FinishHim {
 
-    public static <P,C,S> List<S> fatality(List<P> originalLst,
-          Predicate<P> filterTopLevel,
-          Function<P, List<C>> mapTopLevel,
-          Function<List<C>, List<? extends C>> flatMap,
-          Function<C, S> mapSecondLevel,
-          Comparator<S> comparator) {
-        throw new NotImplementedException("Please complete fr.java.bases.FinishHim#fatality");
+    public static <P, C, S> List<S> fatality(List<P> originalLst,
+            Predicate<P> filterTopLevel,
+            Function<P, List<C>> mapTopLevel,
+            Function<List<C>, List<? extends C>> flatMap,
+            Function<C, S> mapSecondLevel,
+            Comparator<S> comparator) {
+        List<P> filteredElements = Filter.filter(originalLst, filterTopLevel);
+        List<List<C>> visitedCitiesGroupbyperson = Map.map(filteredElements, mapTopLevel);
+        List<C> allVisitedCities = FlatMap.flatMap(filteredElements, mapTopLevel);
+        List<S> countries = Map.map(allVisitedCities, mapSecondLevel);
+        List<S> uniqueCountries = MiscFunctions.distinct(countries);
+        uniqueCountries.sort(comparator);
+        return uniqueCountries;
     }
 }
